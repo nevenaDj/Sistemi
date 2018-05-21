8cea9c312b188091e85e3b2c482905e910def1c5
Nevena Đerić <nevena5695@gmail.com>
2018-05-21 20:50:24 +0200

    drools spring project

---
+++ drools-spring-app/src/main/java/drools/spring/example/DroolsSpringAppApplication.java
@@ -0,0 +1,12 @@
+package drools.spring.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroolsSpringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DroolsSpringAppApplication.class, args);
+	}
+}
